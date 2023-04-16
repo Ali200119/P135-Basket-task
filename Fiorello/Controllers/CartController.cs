6ab10d690035d869bef096741729a074f247688d
Ali200119 <aliit@code.edu.az>
2023-04-16 22:12:47 +0400

    Task complete

---
+++ Fiorello/Controllers/CartController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiorello.Data;
+using Fiorello.Models;
+using Fiorello.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Fiorello.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CartController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            List<CartVM> cart;
+
+            if (Request.Cookies["cart"] != null && Request.Cookies["cart"].Count() != 0)
+            {
+                cart = JsonConvert.DeserializeObject<List<CartVM>>(Request.Cookies["cart"]);
+            }
+
+            else
+            {
+                cart = new List<CartVM>();
+            }
+
+
+            List<CartDetailVM> cartDetails = new();
+
+
+            foreach (var product in cart)
+            {
+                Product dbProduct = await _context.Products.Include(p => p.ProductImages).FirstOrDefaultAsync(p => p.Id == product.Id);
+                cartDetails.Add(new CartDetailVM
+                {
+                    Id = dbProduct.Id,
+                    Name = dbProduct.Name,
+                    Image = dbProduct.ProductImages.Where(pi => pi.IsMain).FirstOrDefault().Name,
+                    Price = dbProduct.Price,
+                    Count = product.Count
+                });
+            }
+
+            return View(cartDetails);
+        }
+
+        [ActionName("Delete")]
+        [HttpPost]
+        public async Task<IActionResult> DeleteProductFromBasket(int? id)
+        {
+            if (id is null) return BadRequest();
+            Product dbProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (dbProduct is null) return NotFound();
+
+            List<CartVM> cart = JsonConvert.DeserializeObject<List<CartVM>>(Request.Cookies["cart"]);
+
+            cart.Remove(cart.FirstOrDefault(cp => cp.Id == id));
+
+            Response.Cookies.Append("cart", JsonConvert.SerializeObject(cart));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IncreaseCount(int? id)
+        {
+            if (id is null) return BadRequest();
+            Product dbProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (dbProduct is null) return NotFound();
+
+            List<CartVM> cart = JsonConvert.DeserializeObject<List<CartVM>>(Request.Cookies["cart"]);
+
+            cart.FirstOrDefault(cp => cp.Id == id).Count++;
+
+            Response.Cookies.Append("cart", JsonConvert.SerializeObject(cart));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DecreaseCount(int? id)
+        {
+            if (id is null) return BadRequest();
+            Product dbProduct = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (dbProduct is null) return NotFound();
+
+            List<CartVM> cart = JsonConvert.DeserializeObject<List<CartVM>>(Request.Cookies["cart"]);
+
+            CartVM product = cart.FirstOrDefault(cp => cp.Id == id);
+
+            if (product.Count > 1)
+            {
+                product.Count--;
+            }
+
+            Response.Cookies.Append("cart", JsonConvert.SerializeObject(cart));
+
+            return Ok();
+        }
+    }
+}
---
+++ Fiorello/wwwroot/js/script.js
@@ -0,0 +1,275 @@
+$(document).ready(function () {
+
+    // HEADER
+
+    $(document).on('click', '#search', function () {
+        $(this).next().toggle();
+    })
+
+    $(document).on('click', '#mobile-navbar-close', function () {
+        $(this).parent().removeClass("active");
+
+    })
+    $(document).on('click', '#mobile-navbar-show', function () {
+        $('.mobile-navbar').addClass("active");
+
+    })
+
+    $(document).on('click', '.mobile-navbar ul li a', function () {
+        if ($(this).children('i').hasClass('fa-caret-right')) {
+            $(this).children('i').removeClass('fa-caret-right').addClass('fa-sort-down')
+        }
+        else {
+            $(this).children('i').removeClass('fa-sort-down').addClass('fa-caret-right')
+        }
+        $(this).parent().next().slideToggle();
+    })
+
+    // SLIDER
+
+    $(document).ready(function () {
+        $(".slider").owlCarousel(
+            {
+                items: 1,
+                loop: true,
+                autoplay: true
+            }
+        );
+    });
+
+    // PRODUCT
+
+    $(document).on('click', '.categories', function (e) {
+        e.preventDefault();
+        $(this).next().next().slideToggle();
+    })
+
+    $(document).on('click', '.category li a', function (e) {
+        e.preventDefault();
+        let category = $(this).attr('data-id');
+        let products = $('.product-item');
+
+        products.each(function () {
+            if (category == $(this).attr('data-id')) {
+                $(this).parent().fadeIn();
+            }
+            else {
+                $(this).parent().hide();
+            }
+        })
+        if (category == 'all') {
+            products.parent().fadeIn();
+        }
+    })
+
+    // ACCORDION 
+
+    $(document).on('click', '.question', function () {
+        $(this).siblings('.question').children('i').removeClass('fa-minus').addClass('fa-plus');
+        $(this).siblings('.answer').not($(this).next()).slideUp();
+        $(this).children('i').toggleClass('fa-plus').toggleClass('fa-minus');
+        $(this).next().slideToggle();
+        $(this).siblings('.active').removeClass('active');
+        $(this).toggleClass('active');
+    })
+
+    // TAB
+
+    $(document).on('click', 'ul li', function () {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        let dataId = $(this).attr('data-id');
+        $(this).parent().next().children('p.active').removeClass('active');
+
+        $(this).parent().next().children('p').each(function () {
+            if (dataId == $(this).attr('data-id')) {
+                $(this).addClass('active')
+            }
+        })
+    })
+
+    $(document).on('click', '.tab4 ul li', function () {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        let dataId = $(this).attr('data-id');
+        $(this).parent().parent().next().children().children('p.active').removeClass('active');
+
+        $(this).parent().parent().next().children().children('p').each(function () {
+            if (dataId == $(this).attr('data-id')) {
+                $(this).addClass('active')
+            }
+        })
+    })
+
+    // INSTAGRAM
+
+    $(document).ready(function () {
+        $(".instagram").owlCarousel(
+            {
+                items: 4,
+                loop: true,
+                autoplay: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    576: {
+                        items: 2
+                    },
+                    768: {
+                        items: 3
+                    },
+                    992: {
+                        items: 4
+                    }
+                }
+            }
+        );
+    });
+
+    $(document).ready(function () {
+        $(".say").owlCarousel(
+            {
+                items: 1,
+                loop: true,
+                autoplay: true
+            }
+        );
+    });
+
+
+
+
+    // WITHOUT REFRESH (AJAX)
+
+    if ($("tbody").children().length == 0) {
+        $("table").addClass("d-none");
+        $(".grand-total").addClass("d-none");
+        $(".alert-warning").removeClass("d-none");
+    }
+
+    // ADD TO CART
+
+    $(document).ready(function () {
+        $(".add-to-cart").click(function () {
+            let id = $(this).parent().parent().attr("data-id");
+
+            $.ajax({
+                type: "POST",
+                url: `home/add/${id}`
+            });
+
+            Swal.fire(
+                'Done!',
+                `${$(this).parent().prev().first().text()} has been added to the cart`,
+                'success'
+            )
+        });
+    });
+
+
+    // DELETE FROM CART
+
+    $(document).ready(function () {
+        $(".delete").click(function () {
+            let id = $(this).parent().parent().attr("data-id");
+
+            $.ajax({
+                type: "POST",
+                url: `cart/delete/${id}`
+            });
+
+            Swal.fire(
+                'Done!',
+                `${$(this).parent().parent().children().eq(1).text()} has been added to the cart`,
+                'success'
+            )
+
+            $(this).parent().parent().remove();
+
+            if ($("tbody").children().length == 0) {
+                $("table").addClass("d-none");
+                $(".grand-total").addClass("d-none");
+                $(".alert-warning").removeClass("d-none");
+            }
+
+            else {
+                let grandTotal = 0;
+
+                for (var value of $("tbody").children()) {
+                    grandTotal += parseFloat($(value).children().eq(4).children().eq(1).text());
+                }
+
+                $(".grand-total").text(`Grand Total: $ ${grandTotal}`);
+            }
+        });
+    });
+
+
+    // INCREASE PRODUCT COUNT
+
+    $(document).ready(function () {
+        $(".plus").click(function() {
+            let id = $(this).parent().parent().attr("data-id");
+
+            $.ajax({
+                type: "POST",
+                url: `cart/increasecount/${id}`
+            });
+
+            let currentCount = parseInt($(this).prev().text());
+
+            currentCount++;
+
+            $(this).prev().text(currentCount);
+
+
+            let price = parseFloat($(this).parent().prev().children().eq(1).text());
+            $(this).parent().next().children().eq(1).text(price * currentCount);
+
+
+            let grandTotal = 0;
+
+            for (var value of $("tbody").children()) {
+                grandTotal += parseFloat($(value).children().eq(4).children().eq(1).text());
+            }
+
+            $(".grand-total").text(`Grand Total: $ ${grandTotal}`);
+        })
+    });
+
+
+    // DECREASE PRODUCT COUNT
+
+    $(document).ready(function () {
+        $(".minus").click(function () {
+            let id = $(this).parent().parent().attr("data-id");
+
+            $.ajax({
+                type: "POST",
+                url: `cart/decreasecount/${id}`
+            });
+
+            let currentCount = parseInt($(this).next().text());
+
+            if (currentCount > 1) {
+                currentCount--;
+
+                $(this).next().text(currentCount);
+
+
+                let price = parseFloat($(this).parent().prev().children().eq(1).text());
+                $(this).parent().next().children().eq(1).text(price * currentCount);
+
+
+                let grandTotal = 0;
+
+                for (var value of $("tbody").children()) {
+                    grandTotal += parseFloat($(value).children().eq(4).children().eq(1).text());
+                }
+
+                $(".grand-total").text(`Grand Total: $ ${grandTotal}`);
+            };
+        })
+    });
+});
